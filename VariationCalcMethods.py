65f7cda473d4057b12bcecd63f58972b69947ad0
eros.abreu <Cemig@2031>
2021-05-11 17:06:41 -0300

    Adicionado calculo soma quadrática das diferenças absolutas das imagens equalizadas

--- VariationCalcMethods.py
+++ VariationCalcMethods.py
@@ -44,6 +44,16 @@ def _calc_equalized_sum(video: str, start: int, end: int, out_queue: Queue):
         last_frame = new_frame
 
 
+def _calc_square_equalized_sum(video: str, start: int, end: int, out_queue: Queue):
+    iter_frame = _iter_frames(video, start, end)
+    last_frame = _processes_equalize_hist(next(iter_frame)[1])
+    for i, new_frame in iter_frame:
+        new_frame = _processes_equalize_hist(new_frame)
+        res = cv2.absdiff(last_frame, new_frame).astype(np.uint8)
+        out_queue.put((i, res.sum()**2))
+        last_frame = new_frame
+
+
 def _process_otsu(frame: np.ndarray):
     return cv2.threshold(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY_INV |
                          cv2.THRESH_OTSU)[1]
@@ -65,10 +75,10 @@ def runner(in_queue: Queue, out_queue: Queue):
         CALC_VARIATIONS_METHODS[method](video, start, end, out_queue)
 
 
-
 CALC_VARIATIONS_METHODS = {
     'OTSU_NON_ZEROS': _calc_otsu_non_zero,
     'EQUALIZED_NON_ZEROS': _calc_equalized_non_zero,
     'EQUALIZED_SUM': _calc_equalized_sum,
+    'SQUARE_EQUALIZED_SUM': _calc_square_equalized_sum,
 }
 
